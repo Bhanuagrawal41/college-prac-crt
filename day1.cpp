2b0f8096ed0f352677dd14f8844df67abc41fb20
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-30 10:04:14 +0530

    day 1

---
+++ day1.cpp
@@ -0,0 +1,17 @@
+// Temprature Conversion 
+// Take input in the form of Celcius and convert it into fahrenheit...
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    double celcius;
+    double fahrenheit;
+    cin >> celcius;
+    fahrenheit = (celcius * 9.0 / 5.0) + 32.0;
+    cout << fahrenheit << endl;
+    
+    return 0;
+    
+}
