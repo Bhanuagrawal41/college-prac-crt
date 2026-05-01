adee35a9e7aa840d577b9eb2e653fe2abe18fbdd
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-01 09:25:37 +0530

    Add age verification for voting eligibility
    Implement age verification for voting eligibility.

---
+++ day 2.cpp
@@ -0,0 +1,13 @@
+// problem 1 -- if condition //
+// two verfiy user age whether eligible for vote or not --//
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age; 
+    cin >> age;
+    if(age >= 18){
+      std::cout << "eligible to vote" << std::endl;
+    }
+    
+}
