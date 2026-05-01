2d9cb2cd1c257f79b3743d4d80ca412eab482348
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-01 09:29:10 +0530

    problem 2 if else

--- day 2.cpp
+++ day 2.cpp
@@ -11,3 +11,20 @@ int main(){
     }
     
 }
+
+
+// problem 1 -- if condition //
+// two verfiy user age whether eligible for vote or not --//
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age; 
+    cin >> age;
+    if(age >= 18){
+      std::cout << "eligible to vote" << std::endl;
+    }else{
+        cout << "not eligible to vote" << endl;
+    }
+    
+}
