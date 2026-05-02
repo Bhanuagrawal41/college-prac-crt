798d49c76b23e4061cdfbc6e591b45fb6af38beb
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-02 09:46:20 +0530

    Implement reverse counting from 10 to 1problem one and two

--- day3.cpp
+++ day3.cpp
@@ -1,1 +1,27 @@
+// problem no 1 ---//
+// print 1 to 10 using a for loop
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+    
+//     for(int i = 1; i <= 10; i++){
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
+
+// problme 2 
+// reverse 10 to 1//
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    for(int i = 10; i>=1; i--){
+        cout << i << " ";
+    }
+    return 0;
+}
