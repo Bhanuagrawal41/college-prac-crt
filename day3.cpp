f6a9bea1fe00c4a5ead5a34deaca4436fd460ecd
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-02 09:55:23 +0530

    Modify main function to count down using do-while
    Updated the main function to count down from 10 to 1.

--- day3.cpp
+++ day3.cpp
@@ -38,3 +38,40 @@ int main(){
     }
     
 }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+//  int main(){
+     
+//      int i = 1;
+     
+//      do{
+//          cout << i << " ";
+//          i++;
+//      }while(i <= 10);
+//      return 0;
+     
+     
+     
+//  }
+ 
+ 
+ #include <iostream>
+using namespace std;
+ int main(){
+     
+     int i = 10;
+     
+     do{
+         cout << i << " ";
+         i--;
+     }while(i >= 1);
+     return 0;
+     
+     
+     
+ }
+
