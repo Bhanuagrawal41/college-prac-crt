177cc747aa861b47f08a21ace90abc57cfac9ffb
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-02 10:09:16 +0530

    Implement factorial calculation in day3.cpp
    Add factorial calculation in main function

--- day3.cpp
+++ day3.cpp
@@ -75,3 +75,23 @@ using namespace std;
      
  }
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int fact = 1;
+    int n;
+    cin >> n;
+
+  for(int i = 1; i<=n; i++ ){
+      fact = fact * i;
+      
+  }
+  cout << fact ;
+    
+    
+    
+}
+
+
