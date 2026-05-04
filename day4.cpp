aaf077263078459fd8a582ecb3c4c9527b039b4c
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-04 11:16:34 +0530

    Implement rectangle printing based on user input
    Added a new function to print a rectangle based on user input for columns and rows.

--- day4.cpp
+++ day4.cpp
@@ -70,3 +70,21 @@ int main(){
     }
     return 0;
 }
+
+// print a recatngle/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int col, row;
+    cin >> col >> row;
+    
+    for(int i = 1; i<= row; i++){
+        for(int j = 1; j <= col; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
