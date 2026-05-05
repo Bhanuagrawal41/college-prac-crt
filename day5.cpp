f3fb548b980c4c922fdf3b8cf3bf85ede49f72e2
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-05 09:42:55 +0530

    Implement star pyramid pattern in day5.cpp
    Add a program to print a pyramid pattern of stars.

--- day5.cpp
+++ day5.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j<=i; j++){
+            std::cout <<  "*";
+        }
+        cout << endl;
+      
+      
+    }
+    
+    for(int k = n-1; k>=1; k--){
+        for(int l = 1; l<=k; l++){
+            cout<<"*";
+        }
+        cout <<"\n";
+    }
+  return 0;
+}
