e95558ab94b9baa4b4e689c8f495fb6070ad4a84
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-07 09:50:32 +0530

    public private protected acesss modiiferes
    Commented out the entire code in day7.cpp, including classes and main functions.

--- day7.cpp
+++ day7.cpp
@@ -1,1 +1,47 @@
+// #include <iostream>
+// using namespace std;
+
+
+// class BankAccount{
+//     private :
+//     int balance = 5000;
+    
+    
+//     public :
+//     void showBalance(){
+//         cout << "blance in your account is : "<< balance;
+//     }
+// };
+
+
+
+// int main(){
+    
+//     BankAccount myaccount;
+//     myaccount.showBalance();
+    
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+
+// class Parent{
+// protected:
+//   int secreateCode = 123;
+// };
+
+// class child : public Parent{
+//     public:
+//     void accesofSecreatecode(){
+//         cout << "secreateCode of the locker is "<< secreateCode;
+//     }
+// };
+
+// int main(){
+//     child obj1;
+//     obj1.accesofSecreatecode();
+// }
 
