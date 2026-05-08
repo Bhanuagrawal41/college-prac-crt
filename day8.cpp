60bdb3a5e292c13ce4cc5ef17849e736a1891568
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-08 09:47:39 +0530

    multilevel inheritance in c++

--- day8.cpp
+++ day8.cpp
@@ -41,3 +41,50 @@ int main(){
     
     
 }
+// problem 2 multilevel inheritance ---//
+
+#include <iostream>
+using namespace std;
+
+class Vehicle{
+    public:
+    void start(){
+        cout << "vehicle is stared vroommm "<< endl;
+        
+    }
+    
+};
+
+
+class Car : public Vehicle{
+    
+    public:
+    void drive(){
+        cout << "car is running" << endl;
+    }
+    
+    
+    
+};
+
+class Speedometer : public Car{
+    public:
+    void speed(){
+        cout << "car is running at 120kmph : " << endl;
+    }
+};
+
+
+
+
+
+
+int main(){
+    Speedometer obj1;
+    obj1.speed();
+    obj1.drive();
+    obj1.start();
+    
+    
+    
+}
