cf263f9d19ab8aca9b8455661adc4cb0e99c26fb
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-08 09:39:58 +0530

    learning the sinlge inheritance

--- day8.cpp
+++ day8.cpp
@@ -1,1 +1,43 @@
+// problem no 1 -- inheritance--//
+// single -- ineheritance --///
 
+
+
+
+#include <iostream>
+using namespace std;
+
+class Vehicle{
+    public:
+    void start(){
+        cout << "vehicle is stared vroommm "<< endl;
+        
+    }
+    
+};
+
+
+class Car : public Vehicle{
+    
+    public:
+    void drive(){
+        cout << "car is running" << endl;
+    }
+    
+    
+    
+};
+
+
+
+
+
+
+int main(){
+    Car ferrai1;
+    ferrai1.start();
+    ferrai1.drive();
+    
+    
+    
+}
