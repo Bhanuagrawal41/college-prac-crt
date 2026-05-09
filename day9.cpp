d81762da5d79ae6e7f25baad9f38afd616336e0b
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-09 10:26:21 +0530

    Update day9.cpp

--- day9.cpp
+++ day9.cpp
@@ -130,3 +130,29 @@ int main(){
     traversal(arr, 5);
     
 }
+
+
+#include <iostream>
+using namespace std;
+// introduction to arrays and traversing in array plus momeory layout//
+
+
+
+
+
+ void traversal(int arr[], int size){
+     
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " " ;
+    }
+}
+
+
+int main(){
+    
+    int arr[] = {10, 20, 30, 40, 50,};
+     
+    traversal(arr, 5);
+    
+}
+
