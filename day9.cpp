63ed8aaa216d149379b1d4f43f63ea9f50a64ac5
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-09 09:58:07 +0530

    Implement constructor overloading in Rectangle class
    Added constructor overloading for the Rectangle class.

--- day9.cpp
+++ day9.cpp
@@ -1,1 +1,45 @@
 // hello
+// problem no 2 constructer overlaoding--//
+
+
+#include <iostream>
+using namespace std;
+
+
+class Rectangle {
+    public:
+    int lenghth;
+    int width;
+    Rectangle(){
+        lenghth = 1;
+        width = 1;
+        
+    }
+    
+    Rectangle(int side){
+        lenghth = side;
+        width = side;
+        
+        
+    }
+    Rectangle(int side1, int side2){
+        
+        lenghth = side1;
+        width = side2;
+        
+    }
+    
+    void showArea(){
+        cout << lenghth*width << endl;
+    }
+};
+
+int main(){
+    Rectangle R1;
+    Rectangle R2(5);
+    Rectangle R3(5,6);
+    
+    R1.showArea();
+    R2.showArea();
+    R3.showArea();
+}
