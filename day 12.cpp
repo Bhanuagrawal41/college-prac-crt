68f9c06855e000e28f442fed8df2efd1cbd4d1bf
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-12 10:41:01 +0530

    Update day 12.cpp

--- day 12.cpp
+++ day 12.cpp
@@ -21,3 +21,28 @@ void printNumber(int n){
 int main(){
     printNumber(1);
 }
+
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if( n == 0 || n == 1){
+        return 1;
+    } 
+        
+        
+        
+        
+        return n* factorial(n-1);
+        
+        
+    
+    
+}
+
+int main(){
+   cout << factorial(5);
+
+    
+}
