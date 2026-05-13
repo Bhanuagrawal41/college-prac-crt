d2c69827d9bb480114de17dd3bd60416a71374d7
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-13 08:49:26 +0530

    concecutive one is not alloweed

--- day 13.cpp
+++ day 13.cpp
@@ -1,1 +1,33 @@
 
+
+#include <iostream>
+using namespace std;
+
+ int consecutiveOnes(int number){
+    if(number == 0){
+        return 0;
+        
+    }
+    
+    
+    if(number == 1){
+        return 2;
+        
+    }
+    if(number == 2){
+        return 3;
+    }
+    
+    
+    //logic
+    
+    return consecutiveOnes(number-1)+ consecutiveOnes(number-2);
+}
+
+
+
+
+
+int main(){
+    cout << consecutiveOnes(4);
+}
