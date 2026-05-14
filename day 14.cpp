f07d892cc3e8a556ab7244f5ca22a126551ec610
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-14 10:50:31 +0530

    Implement linked list with deleteAthead and display
    Added a linked list implementation with delete and display functions.

--- day 14.cpp
+++ day 14.cpp
@@ -27,3 +27,55 @@ void traversal(){
         cout << head->data
         
     }
+
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    
+    int data;
+    Node* next;
+    
+    Node(int newdata){
+      this->data = newdata;
+      this->next = nullptr;
+    }
+};
+
+// 
+Node* head = nullptr;
+  void deleteAthead(){
+      if(head == nullptr){
+          cout << "list is empty";
+          return;
+      }
+      head = head->next;
+  }
+void display(){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << "null";
+}
+
+
+
+
+
+int main()
+{
+   head = new Node(10);
+   
+   head->next = new Node(20);
+   head->next->next = new Node(30);
+   head->next->next->next = new Node(40);
+   deleteAthead();
+   display();
+ 
+
+  return 0;
+}
